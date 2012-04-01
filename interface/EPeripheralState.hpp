85685dd1dd803e60638d3edf3e4b785e7fc64827
Artur Stepniak <aart.st@gmail.com>
2012-04-01 15:11:09 +0200

    Implemented interrupts for USART

---
+++ interface/EPeripheralState.hpp
@@ -0,0 +1,21 @@
+/*
+ * EPeripheralState.hpp
+ *
+ *  Created on: Apr 1, 2012
+ *      Author: artur
+ */
+
+#ifndef EPERIPHERALSTATE_HPP_
+#define EPERIPHERALSTATE_HPP_
+
+enum EPeripheralState
+{
+	EPeripheralStateUnusable,
+	EPeripheralStateUsable,
+	EPeripheralStateRunning
+};
+
+typedef EPeripheralState	EPeripheralState;
+
+
+#endif /* EPERIPHERALSTATE_HPP_ */
