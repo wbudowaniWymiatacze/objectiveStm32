5eb416ed1951d0669765b14dadea0eacba4dc21f
Michal <watan@poczta.wp.pl>
2012-04-17 20:45:12 +0200

    activation of APB2ENR for SD detect GPIO PB9.

--- SD/inc/sd.h
+++ SD/inc/sd.h
@@ -15,23 +15,5 @@
 //Initialize card insert
 void card_insert_init(void);
 
-/** 
-  * @brief Reset and Clock Control
-  */
-
-typedef struct
-{
- uint32_t CR;
- uint32_t CFGR;
- uint32_t CIR;
- uint32_t APB2RSTR;
- uint32_t APB1RSTR;
- uint32_t AHBENR;
- uint32_t APB2ENR;
- uint32_t APB1ENR;
- uint32_t BDCR;
- uint32_t CSR;
-} RCC_TypeDef2;
-
 #endif	/* SD_H */
 
