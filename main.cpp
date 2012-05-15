bddc423c00550f280b6ea100a7b0989197fbd5c3
Artur Stepniak <aart.st@gmail.com>
2012-05-15 20:10:52 +0200

    fix in main.cpp to make master built

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 
 #include <CGpioManager.hpp>
 #include <CRccManager.hpp>
-#include <CUsart.hpp>
 #include <TypePeriph.hpp>
 
 uint16_t g_usartReadBuffer[ 100 ];
@@ -18,34 +17,6 @@ int main()
 {
 	CGpioManager gpioMngr;
 	CRccManager rccMngr;
-	SUsartConfig usartConfStruct;
-	USART_InitTypeDef usartConf = usartConfStruct.usartConfig;
-	CUsart< SUsart1 > usart1( gpioMngr, rccMngr );
-
-	usartConf.USART_BaudRate = 115200;
-	usartConf.USART_WordLength = USART_WordLength_8b;
-	usartConf.USART_StopBits = USART_StopBits_1;
-	usartConf.USART_Parity = USART_Parity_No;
-	usartConf.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	usartConf.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-	GPIO_InitTypeDef gpiosConfig[ 2 ];
-        
-        //@todo TODO
-        // mode dla USART Rx i Tx są określone - wywalić konieczność ustawiania GPIO_Mode,
-	// speed jako argument CUsart::init z wartością domyślną
-	gpiosConfig[ 0 ].GPIO_Pin = 0;
-	gpiosConfig[ 0 ].GPIO_Speed = GPIO_Speed_50MHz;
-	gpiosConfig[ 0 ].GPIO_Mode = GPIO_Mode_IN_FLOATING;
-
-	gpiosConfig[ 1 ].GPIO_Pin = 0;
-	gpiosConfig[ 1 ].GPIO_Speed = GPIO_Speed_50MHz;
-	gpiosConfig[ 1 ].GPIO_Mode = GPIO_Mode_AF_PP;
-
-	usart1.init( gpiosConfig, &usartConfStruct );
-
-	uint16_t buffer[ 50 ] = { 0 };
-	usart1.write( buffer, 50 );
 
 	return 0;
 }
